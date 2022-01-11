a624d76be36101e5da1a126f45f84cb9e42a66c2
Honey Tyagi <honeyt290@gmail.com>
2022-01-11 20:09:49 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// const imgBx = document.querySelector(".imgBx");
+// const slides = imgBx.getElementsByTagName("img");
+// var i = 0;
+
+// function nextSlide() {
+//   slides[i].classList.remove("active");
+//   i = (i + i) % slides.length;
+//   slides[i].classList.add("active");
+// }
+// function PrevSlide() {
+//   slides[i].classList.remove("active");
+//   i = (i - i + slides.length) % slides.length;
+//   slides[i].classList.add("active");
+// }
+
+
+// $(document).ready(function(){
+//         $('.customer-logos').slick({
+//             slidesToShow: 6,
+//             slidesToScroll: 1,
+//             autoplay: true,
+//             autoplaySpeed: 1500,
+//             arrows: false,
+//             dots: false,
+//             pauseOnHover:false,
+//             responsive: [{
+//                 breakpoint: 768,
+//                 setting: {
+//                     slidesToShow:4
+//                 }
+//             }, {
+//                 breakpoint: 520,
+//                 setting: {
+//                     slidesToShow: 3
+//                 }
+//             }]
+//         });
+//     });
+
